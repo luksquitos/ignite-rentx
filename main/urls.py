aa916cc43f211410d8f39ad006340b8c5d33f58f
Lucas <lucasmartinscaet@hotmail.com>
2023-01-21 15:59:53 -0300

    Arrumando o static para a api retornar as imagens

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,7 @@
 import rest_framework
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from cars.api.viewsets import CarroViewset
@@ -12,6 +14,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('', include(router.urls))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
